123f7451f905a728a1b85653da0ab721507e55a1
Bithrop <tjl9567@yahoo.com>
2020-02-20 02:25:45 -0500

    Second game

--- HW0/js/main.js
+++ HW0/js/main.js
@@ -50,7 +50,7 @@ function make_main_game_state( game )
     
     function update() {
 		
-		game.physics.arcade.overlap(bouncy, luigi, killLuigi, null, this);
+		game.physics.arcade.overlap(bouncy, luigi, luigi.kill(), null, this);
 		
         // Accelerate the 'logo' sprite towards the cursor,
         // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
