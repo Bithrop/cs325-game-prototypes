880cd8fc8c1f3f0a1e9f5176f76f87426cb51e3f
Bithrop <tjl9567@yahoo.com>
2020-02-19 22:53:49 -0500

    Second game

--- HW1/js/main.js
+++ HW1/js/main.js
@@ -68,7 +68,7 @@ window.onload = function() {
         // so it will be truly centered.
         bouncy.anchor.setTo( 0.5, 0.5 );
         
-		bouncy.scale.setTo(0.5,0.5);
+		//bouncy.scale.setTo(0.5,0.5);
 		//bouncy 2 for testing collide
 		bouncy2 = game.add.sprite( 300, 400, 'logo' );
 		
