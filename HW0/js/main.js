d2cade833e032ecb0ec0232f4af99a475975dad0
Bithrop <tjl9567@yahoo.com>
2020-02-20 02:43:03 -0500

    Second game

--- HW0/js/main.js
+++ HW0/js/main.js
@@ -62,7 +62,7 @@ function make_main_game_state( game )
     return { "preload": preload, "create": create, "update": update };
 }
 
-    function killLuigi(player, luigi) 
+    function killLuigi() 
 	{
 		
 		luigi.kill();
