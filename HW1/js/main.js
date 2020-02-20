5409804c2f839a936fcf0ad665a6aef294bbca07
Bithrop <tjl9567@yahoo.com>
2020-02-20 00:44:33 -0500

    Second game

--- HW1/js/main.js
+++ HW1/js/main.js
@@ -46,7 +46,7 @@ window.onload = function() {
         
         //add tiles
         map.addTilesetImage('tiles');
-		//map.addTilesetImage('fieldTiles');
+		map.addTilesetImage('fieldTiles');
 		
         //test colider
 		map.setCollisionBetween(0,1, true, layer1);
