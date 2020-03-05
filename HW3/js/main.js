5376632ac63cab787dfc4d3d95170c85f30a6be9
Bithrop <tjl9567@yahoo.com>
2020-03-04 19:45:43 -0500

    Assignment 4

--- HW3/js/main.js
+++ HW3/js/main.js
@@ -26,7 +26,7 @@ function make_main_game_state( game )
         var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
         var text = game.add.text( game.world.centerX, 15, "Test test.", style );
         text.anchor.setTo( 0.5, 0.0 );
-		cursors = game.input.keyboard.creatCursorKeys();
+		cursors = game.input.keyboard.createCursorKeys();
     }
     
     function update() {
