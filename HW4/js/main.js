4092a6c208da8eb401d7cdb3e4ebdf51dcfccf70
Bithrop <tjl9567@yahoo.com>
2020-04-09 07:18:00 -0400

    Game 5?

--- HW4/js/main.js
+++ HW4/js/main.js
@@ -68,8 +68,8 @@ function make_main_game_state( game )
 		
 		
 		crow = game.add.sprite( 500, 500, 'crow' );
-		game.physics.enable( crow, Phaser.Physics.ARCADE);
-		game.physics.arcade.moveToXY(crow),0,0,400)'
+		game.physics.enable(crow, Phaser.Physics.ARCADE);
+		game.physics.arcade.moveToXY(crow,0,0,400)'
     }
     
 	
