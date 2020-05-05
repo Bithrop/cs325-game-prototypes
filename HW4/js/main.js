102173280bf056880fb22a8807a19d282602e267
Bithrop <tjl9567@yahoo.com>
2020-05-05 08:31:22 -0400

    Game 7

--- HW4/js/main.js
+++ HW4/js/main.js
@@ -12,7 +12,7 @@ function make_main_game_state( game )
 		game.load.spritesheet('farmer', 'assets/oldman_walk_sheet.png', 64, 64);
 		game.load.image('bullet', 'assets/new_bullet.png');
 		game.load.image('crow', 'assets/raven-black0001.png');
-		game.load.image('peas', 'assets/peas.png' );
+		game.load.image('peas', 'assets/peas1.png' );
     }
     
     var bouncy;
